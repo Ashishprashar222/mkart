3966102952d5125000ed0a64f125e506cc1ab2a2
Ashish Prashar <ashishprashar37@gmail.com>
2020-08-13 21:33:19 +0530

    Update settings.py

--- mkart/settings.py
+++ mkart/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's^+49k&^i-*i-!uqzt0hcc)@j0w-o1nr2vsk2m)7znz!ylnz8g'
+SECRET_KEY = 'give the secret key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
